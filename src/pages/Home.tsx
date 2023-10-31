77e06d20ca9b42448ef29feb2e26d9ad9efd40a3
nyzs <neazenmeh@gmail.com>
2023-10-31 16:22:21 +0100

    not important/minor test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import Logo from "../components/Main/Logo";
+
 const Home = () => {
   return (
     <div>
       <h1 className="h-96">hello guys</h1>
+      <h1>this is a minor test dont mind it</h1>
+      <Logo />
     </div>
   );
 };
