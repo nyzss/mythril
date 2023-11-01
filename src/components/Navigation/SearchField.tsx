29bfa4c6646d58978e1374de8e57ed256fd3d58f
nyzs <neazenmeh@gmail.com>
2023-11-01 02:22:53 +0100

    ignore

--- src/components/Navigation/SearchField.tsx
+++ src/components/Navigation/SearchField.tsx
@@ -7,6 +7,7 @@ const SearchField = () => {
   const [search, setSearch] = useState<string>("");
   const handleSubmit = () => {
     navigate("/search/" + search);
+    setSearch("");
   };
   return (
     <>
